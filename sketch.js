6544828edae0f59e4088e5f1738eec2af6dbd3e3
M Sami <77672838+MSami625@users.noreply.github.com>
2021-03-29 17:01:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -71,8 +71,8 @@ function draw() {
   textSize(20);
   fill("lightyellow");
   text("Drag the polygon to destroy the blocks",300,30);
-  textSize(10);
-  text("Press Space to get a second Chance to Play!!",650 ,350);
+  textSize(20);
+  text("Press Space to get a second Chance to Play!!",290 ,350);
   ground.display();
   stand1.display();
   stand2.display();
